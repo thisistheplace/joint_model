72c1722b9491388d07652a3e9127930f68400f05
thisistheplace <57633545+thisistheplace@users.noreply.github.com>
2022-09-15 22:12:07 +0100

    Feature/continuous mesh (#18)
    * Fixed tests and some refactoring
    
    * Updated messaging
    
    * changes
    
    * Merge
    
    * Updated yaml
    
    Co-authored-by: Ben Cannell <bencannell@bens-air.home>

--- src/app/modelling/geometry/weld.py
+++ src/app/modelling/geometry/weld.py
@@ -1,7 +1,10 @@
+from copy import deepcopy
 import math
 from math import cos, sin, sqrt
+import sympy
 
 from .vectors import angle_between_vectors
+from .intersections import plane_intersect, circle_intersect, IntersectionError
 from ...interfaces import *
 
 # TODO: look into https://mathcurve.com/courbes2d.gb/alain/alain.shtml
@@ -19,8 +22,37 @@ def z(r1: float, r2: float, phi: float, pheta: float) -> float:
             sin(phi)
 
 
-def get_weld_intersect_points():
+def get_weld_intersect_points(master: NpTubular, slave: NpTubular):
 
     for degrees in range(0, 361, 10):
         angle = degrees * math.pi / 360
-        
+
+        # calculate size of 
+
+        # Get 2D point of intersection with the circle
+        intersect2D_array = circle_intersect(master.axis.point.array, master.diameter, slave.axis.point.array, slave.axis.vector.array)
+
+        if len(intersect2D_array) == 0:
+            raise IntersectionError(f"No intersections found for {slave.name} on {master.name} at angle {angle}")
+
+        # Use point to determine side of circle intersect to use
+        for intersect2D in intersect2D_array:
+            intersect_vector = intersect2D - master.axis.point.array[:2]
+            point_vector = slave.axis.point.array[:2] - master.axis.point.array[:2]
+            angle2D = angle_between_vectors(intersect_vector, point_vector)
+            if angle2D <= math.pi / 2:
+                break
+
+        # Determine intersection of vector with plane
+        plane_point = np.array(
+            [intersect2D[0], intersect2D[1], slave.axis.point.array[2]]
+        )
+        p2 = deepcopy(plane_point)
+        p2[0] += 1.0
+        p3 = deepcopy(plane_point)
+        p3[2] += 1.0
+        plane: sympy.Plane = sympy.Plane(
+            sympy.Point3D(plane_point), sympy.Point3D(p2), sympy.Point3D(p3)
+        )
+        intersect3D = plane_intersect(slave.axis.vector.array, plane_point, plane)
+        # intersects[slave.name] = np.array(intersect3D, dtype=float)
