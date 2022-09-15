4ae16a0d3d5620e81af05ea9af0df6e84ad7ceda
Ben Cannell <bencannell@bens-air.home>
2022-09-15 22:10:52 +0100

    Merge

--- src/app/modelling/geometry/weld.py
+++ src/app/modelling/geometry/weld.py
@@ -1,11 +1,10 @@
+from copy import deepcopy
 import math
 from math import cos, sin, sqrt
+import sympy
 
 from .vectors import angle_between_vectors
-<<<<<<< HEAD
 from .intersections import plane_intersect, circle_intersect, IntersectionError
-=======
->>>>>>> main
 from ...interfaces import *
 
 # TODO: look into https://mathcurve.com/courbes2d.gb/alain/alain.shtml
@@ -23,7 +22,6 @@ def z(r1: float, r2: float, phi: float, pheta: float) -> float:
             sin(phi)
 
 
-<<<<<<< HEAD
 def get_weld_intersect_points(master: NpTubular, slave: NpTubular):
 
     for degrees in range(0, 361, 10):
@@ -57,11 +55,4 @@ def get_weld_intersect_points(master: NpTubular, slave: NpTubular):
             sympy.Point3D(plane_point), sympy.Point3D(p2), sympy.Point3D(p3)
         )
         intersect3D = plane_intersect(slave.axis.vector.array, plane_point, plane)
-        intersects[slave.name] = np.array(intersect3D, dtype=float)
-=======
-def get_weld_intersect_points():
-
-    for degrees in range(0, 361, 10):
-        angle = degrees * math.pi / 360
-        
->>>>>>> main
+        # intersects[slave.name] = np.array(intersect3D, dtype=float)
